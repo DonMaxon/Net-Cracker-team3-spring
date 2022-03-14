69e47c111c0371da03a4fae116c4fa9655c14699
Maxim Tislenko <makstislenko@gmail.com>
2022-03-14 21:28:12 +0300

    Controllers added

--- src/main/java/com/example/demo/basicClasses/controllers/ServiceController.java
+++ src/main/java/com/example/demo/basicClasses/controllers/ServiceController.java
@@ -3,8 +3,11 @@ package com.example.demo.basicClasses.controllers;
 import com.example.demo.basicClasses.services.ServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.demo.basicClasses.entity.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.UUID;
 
 @Controller
@@ -18,11 +21,11 @@ public class ServiceController {
         serviceService.delete(id);
     }
 
-    @GetMapping("/service/get")
+    @DeleteMapping("/service/get")
     public void getService(UUID id){
         serviceService.findById(id);
     }
-    @GetMapping("/service/зщые")
+    @PostMapping("/service/post")
     public void postService(Service service){
         serviceService.save(service);
     }
